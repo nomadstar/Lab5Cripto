cd0efba24ab5cd65981169c9752a8503c3096f0a
N0m4d <108416813+nomadstar@users.noreply.github.com>
2024-12-02 20:10:49 -0300

    Let's go

--- server.sh
+++ server.sh
@@ -1,18 +1,21 @@
+apt-get install -y openssh-server
 USER=prueba
 PASS='prueba'
 useradd -m -s /bin/bash $USER && echo "$USER:$PASS" | chpasswd
 
 if [ "$1" = true ]; then
-    apt-get install -y wget g++ make zlib1g-dev libssl-dev
-    wget https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.0p1.tar.gz && \
-        tar -xzf openssh-9.0p1.tar.gz
-    cd openssh-9.0p1 && \
-        sed -i 's/^#define SSH_VERSION.*/#define SSH_VERSION "OpenSSH_?"/' version.h && \
-        ./configure && \
-        make && \
-        make install
-else
-    apt-get install -y openssh-server
+    apt-get install -y wget g++ make zlib1g-dev libssl-dev build-essential libpam0g-dev libselinux1-dev libaudit-dev tcpdump
+    wget https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.0p1.tar.gz
+    tar -xzf openssh-9.0p1.tar.gz
+    cd openssh-9.0p1
+    sed -i 's/^#define SSH_VERSION.*/#define SSH_VERSION "OpenSSH_?"/' version.h
+    sleep 5
+    ./configure
+    sleep 5
+    make
+    sleep 5
+    make install
 fi
+
 
 tail -f /dev/null
